72095c072e472fc4c34b93f3dbe6386bf0c8ce8a
Ganesh1617 <151633666+Ganesh1617@users.noreply.github.com>
2023-12-14 09:02:38 +0530

    Initial commit

---
+++ PrimeTest/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication11
+{
+    class Program
+    {
+        void Display()
+        {
+            int cnt = 0, a;
+            Console.WriteLine("Enter Number");
+            a = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < a; i++)
+            {
+                if (a % 2 == 0)
+                {
+                    cnt++; break;
+                }
+            }
+                if (cnt == 0)
+                {
+                    Console.WriteLine("Prime Number");
+                }
+                else
+                {
+                    Console.WriteLine("Not a Prime Number");
+                }
+        }
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Display();
+            Console.ReadLine();
+        }
+    }
+}
